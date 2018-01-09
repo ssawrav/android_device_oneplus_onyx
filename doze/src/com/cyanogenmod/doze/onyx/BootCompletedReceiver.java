3e4432cdf87a0228bfb3418945f0c2176a5a7356
ssawrav <s.sawrav@gmail.com>
2018-01-09 18:18:28 +0530

    Revert "Change package name for uniformity"
    This reverts commit 8ec07f06e7b4e0118b4c5b2e7437bf7a8f15845c.

--- doze/src/com/cyanogenmod/doze/onyx/BootCompletedReceiver.java
+++ doze/src/com/cyanogenmod/doze/onyx/BootCompletedReceiver.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.custom.ambient.display;
+package com.cyanogenmod.doze.onyx;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
--- doze/src/com/cyanogenmod/doze/onyx/WakeUpGestureSettings.java
+++ doze/src/com/cyanogenmod/doze/onyx/WakeUpGestureSettings.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.custom.ambient.display;
+package com.cyanogenmod.doze.onyx;
 
 import android.app.ActionBar;
 import android.os.Bundle;
