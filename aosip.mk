6ab902b62d205ea99c52a64bab4582417146f4ff
ssawrav <s.sawrav@gmail.com>
2018-01-09 18:16:32 +0530

    mka Kronic
    Signed-off-by: ssawrav <s.sawrav@gmail.com>

--- aosip.mk
+++ aosip.mk
@@ -18,13 +18,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from onyx device
 $(call inherit-product, device/oneplus/onyx/device.mk)
 
-# Inherit some common Pegasus stuff.
-$(call inherit-product, vendor/pos/config/common_full_phone.mk)
+# Inherit some common Kronic stuff.
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
 # Call the proprietary setup
 $(call inherit-product-if-exists, vendor/oneplus/onyx/onyx-vendor.mk)
 
-PRODUCT_NAME := pos_onyx
+PRODUCT_NAME := aosip_onyx
 PRODUCT_DEVICE := onyx
 PRODUCT_MANUFACTURER := OnePlus
 
