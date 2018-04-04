141b3f2f01720307d643306ce2c0f8cdf2d2ae66
ssawrav <s.sawrav@gmail.com>
2018-04-04 23:38:12 +0530

    Add back Build Fingerprint
    Signed-off-by: ssawrav <s.sawrav@gmail.com>

--- aosip.mk
+++ aosip.mk
@@ -35,4 +35,8 @@ TARGET_VENDOR := oneplus
 TARGET_VENDOR_PRODUCT_NAME := onyx
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-	DEVICE_MAINTAINERS="Saket Sawrav(I-m-mortal)"
+    TARGET_DEVICE=OnePlus \
+    PRODUCT_NAME=OnePlus \
+    BUILD_FINGERPRINT="OnePlus/OnePlus/OnePlus:6.0.1/MMB29M/10281213:user/release-keys" \
+    PRIVATE_BUILD_DESC="OnePlus-user 6.0.1 MMB29M 10281213 release-keys"
+
